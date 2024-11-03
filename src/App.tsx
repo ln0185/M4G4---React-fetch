accc27708409df9733e93cf4fae4e681c10a227f
ln0185 <helene.delaunayratel@gmail.com>
2024-11-03 16:44:44 +0000

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import LaughAndLearn from "./components/LaughAndLearn";
 const App: React.FC = () => {
   return (
     <div>
-      <h1 style={{ textAlign: "center", margin: "1rem 0" }}>Laugh & Learn</h1>
+      <h1 style={{ textAlign: "center", margin: "1rem 0" }}>
+        An advice and a cute dog
+      </h1>
       <LaughAndLearn />
     </div>
   );
